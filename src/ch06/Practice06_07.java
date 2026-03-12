f4ca620430ec956bea021d4dc831f6d277a7535c
Seungwon55 <blackstar55@naver.com>
2026-03-12 13:53:47 +0900

    feat: 6-2~6-7 연습문제 풀이

---
+++ src/ch06/Practice06_07.java
@@ -0,0 +1,23 @@
+package ch06;
+
+public class Practice06_07 {
+    public static void main(String[] args) {
+        MyPoint p = new MyPoint(1, 1);
+
+        System.out.println(p.getDistance(2, 2));
+    }
+}
+
+class MyPoint {
+    int x;
+    int y;
+
+    MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double getDistance(int x1, int y1) {
+        return Math.sqrt(Math.pow(x, x1) + Math.pow(y, y1));
+    }
+}
