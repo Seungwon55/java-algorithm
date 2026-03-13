264ba78bf31471bb58c4de9924fe477b72bb0101
Seungwon55 <blackstar55@naver.com>
2026-03-13 12:09:12 +0900

    feat: 9-7~9-10 연습문제 풀이

---
+++ src/ch09/Practice09_07.java
@@ -0,0 +1,12 @@
+package ch09;
+
+public class Practice09_07 {
+    public static void main(String[] args) {
+        System.out.println(contains("12345", "23"));
+        System.out.println(contains("12345", "67"));
+    }
+
+    static boolean contains(String src, String target) {
+        return src.contains(target);
+    }
+}
